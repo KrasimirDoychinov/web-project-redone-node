d4b5b1c41ed380bc9b4bcffdfa92e5734c28f6e6
Krasimir Doychinov <detha4545@gmail.com>
2022-10-11 16:51:14 +0300

    fix textarea size

--- static/js/script.js
+++ static/js/script.js
@@ -2,6 +2,16 @@ const host = 'http://localhost:3000/api/';
 
 addEventListener('DOMContentLoaded', function () {
 	document.querySelector('body').classList.remove('preload');
+
+	document.querySelectorAll('textarea').forEach((x) => {
+		x.style.height = 'auto';
+		x.style.height = x.scrollHeight + 'px';
+
+		x.addEventListener('input', (e) => {
+			e.currentTarget.style.height = 'auto';
+			e.currentTarget.style.height = e.currentTarget.scrollHeight + 'px';
+		});
+	});
 });
 
 document.querySelectorAll('.clickable-td').forEach((x) => {
